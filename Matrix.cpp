15738906a8082e6a05beca00b240019fd14fa450
shra7vani <158210295+shra7vani@users.noreply.github.com>
2024-04-30 10:07:11 +0530

    Add files via upload

---
+++ Matrix.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class Date
+{
+    private:
+    int day;
+    int month;
+    int year;
+    
+    public:
+    Date(int d,int m,int y)
+    {
+        day=d;
+    month=m;
+        year=y;
+    }
+   void setDate(int d,int m,int y)
+    {        day=d;
+    month=m;
+        year=y;
+
+    }
+    void displayDate(){
+        cout<<"Date:"<<day<<"/ "<<month<<" /"<<year<<"/ "<<endl;
+    }
+    
+};
+    int main(){
+        
+        Date date(16,3,2024);
+       date.displayDate();
+       return 0;
+    }
+    
+    
